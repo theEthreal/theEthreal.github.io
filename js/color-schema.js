3de5d7671d0d3591858ca414dafc4c938c24f8b3
duyuan <duyuan070759@163.com>
2022-05-22 16:50:21 +0800

    Site updated: 2022-05-22 16:50:20

--- js/color-schema.js
+++ js/color-schema.js
@@ -1,4 +1,4 @@
-/* global Fluid */
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>/* global Fluid */
 
 /**
  * Modified from https://blog.skk.moe/post/hello-darkmode-my-old-friend/
--- js/leancloud.js
+++ js/leancloud.js
@@ -1,4 +1,4 @@
-/* global CONFIG */
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>/* global CONFIG */
 
 (function(window, document) {
   // 查询存储的记录
